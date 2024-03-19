c5b97731565b1ef2ecc80cb0e1786bd713926585
MohammedBagowabair <159595929+MohammedBagowabair@users.noreply.github.com>
2024-03-20 03:35:27 +0800

    Create Villa Table

--- Bookly.Infrastructure/Data/ApplicationDbContext.cs
+++ Bookly.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Bookly.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,6 @@ namespace Bookly.Infrastructure.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
         {
         }
+        public DbSet<Villa> Villas { get; set; }
     }
 }
