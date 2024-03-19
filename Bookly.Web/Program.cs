2f1c5d4f87a794e0054613baabd0746605e5dda6
MohammedBagowabair <159595929+MohammedBagowabair@users.noreply.github.com>
2024-03-20 03:26:37 +0800

    Create Database

--- Bookly.Web/Program.cs
+++ Bookly.Web/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-//- Register ApplicationDbContext to program class file
+//- Register ApplicationDbContext to program class file 
 builder.Services.AddDbContext<ApplicationDbContext>(option =>
 option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
