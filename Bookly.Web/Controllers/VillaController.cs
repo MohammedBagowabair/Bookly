115d96ba62295eb0f1491fc7b91dcbdc5887e305
MohammedBagowabair <159595929+MohammedBagowabair@users.noreply.github.com>
2024-03-20 05:26:05 +0800

    Create Villa UI

--- Bookly.Web/Controllers/VillaController.cs
+++ Bookly.Web/Controllers/VillaController.cs
@@ -16,5 +16,10 @@ namespace Bookly.Web.Controllers
             var villas = _db.Villas.ToList();
             return View(villas);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
