0e86ba286b3c1832e769db5d5aec2a3ad133dabd
MohammedBagowabair <159595929+MohammedBagowabair@users.noreply.github.com>
2024-03-21 00:46:30 +0800

    Part 2 - Update in Action

--- Bookly.Web/Controllers/VillaController.cs
+++ Bookly.Web/Controllers/VillaController.cs
@@ -49,5 +49,17 @@ namespace Bookly.Web.Controllers
             }
             return View(obj);
         }
+
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        {
+            if (ModelState.IsValid && obj.Id > 0)
+            {
+                _db.Villas.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
