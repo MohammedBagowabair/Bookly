3a058c314be477818a6953f130d97aed808ad089
MohammedBagowabair <159595929+MohammedBagowabair@users.noreply.github.com>
2024-03-21 03:27:59 +0800

    Part 3 - Update Post in Action

--- Bookly.Web/Controllers/VillaNumberController.cs
+++ Bookly.Web/Controllers/VillaNumberController.cs
@@ -80,16 +80,22 @@ namespace Bookly.Web.Controllers
         }
 
         [HttpPost]
-        public IActionResult Update(Villa obj)
+        public IActionResult Update(VillaNumberVM villaNumberVM)
         {
-            if (ModelState.IsValid && obj.Id > 0)
+            if (ModelState.IsValid)
             {
-                _db.Villas.Update(obj);
+                _db.VillaNumbers.Update(villaNumberVM.VillaNumber);
                 _db.SaveChanges();
-                TempData["success"] = "The villa has been updated successfully.";
+                TempData["success"] = "The villa Number has been Updated successfully.";
                 return RedirectToAction("Index");
             }
-            return View();
+            villaNumberVM.VillaList = _db.Villas.ToList().Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            });
+
+            return View(villaNumberVM);
         }
 
         public IActionResult Delete(int villaId)
