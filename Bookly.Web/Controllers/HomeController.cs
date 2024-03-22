279540b88b4f70b4a2ce17b95555466713cb4ca9
MohammedBagowabair <159595929+MohammedBagowabair@users.noreply.github.com>
2024-03-23 00:46:41 +0800

    Part 5 -Working on Home UI & Villa Details

--- Bookly.Web/Controllers/HomeController.cs
+++ Bookly.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
+using Bookly.Application.Common.Interfaces;
 using Bookly.Web.Models;
+using Bookly.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,16 +8,22 @@ namespace Bookly.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IUnitOfWork unitOfWork)
         {
-            _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            HomeVM homeVM = new()
+            {
+                VillaList = _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity"),
+                Nights = 1,
+                CheckInDate = DateOnly.FromDateTime(DateTime.Now)
+            };
+            return View(homeVM);
         }
 
         public IActionResult Privacy()
