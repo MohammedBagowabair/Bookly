ce07f8c2ccb64cf7748c442e22c8e158b9e5a8ad
MohammedBagowabair <159595929+MohammedBagowabair@users.noreply.github.com>
2024-03-23 01:35:28 +0800

    Part 5 - Add Account Controller and Inject Helper Dependencies

---
+++ Bookly.Web/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using Bookly.Application.Common.Interfaces;
+using Bookly.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookly.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(IUnitOfWork unitOfWork,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _roleManager = roleManager;
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
