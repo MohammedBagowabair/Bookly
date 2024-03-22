b58ae13ef906bf185e0e0032aba15014df846f96
MohammedBagowabair <159595929+MohammedBagowabair@users.noreply.github.com>
2024-03-23 02:23:45 +0800

    Part 5 - Login Post Endpoint

--- Bookly.Web/Controllers/AccountController.cs
+++ Bookly.Web/Controllers/AccountController.cs
@@ -36,6 +36,42 @@ namespace Bookly.Web.Controllers
             return View(loginVM);
         }
 
+
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager
+                    .PasswordSignInAsync(loginVM.Email, loginVM.Password, loginVM.RememberMe, lockoutOnFailure: false);
+
+
+                if (result.Succeeded)
+                {
+
+
+                    if (string.IsNullOrEmpty(loginVM.RedirectUrl))
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        return LocalRedirect(loginVM.RedirectUrl);
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid login attempt.");
+                }
+            }
+
+            return View(loginVM);
+        }
+
+
+
+
         public IActionResult Register()
         {
             if (!_roleManager.RoleExistsAsync(SD.Role_Admin).GetAwaiter().GetResult())
