7c4fd48ba4fca0bdc29f8c300eb45ad2e151a0a4
MohammedBagowabair <159595929+MohammedBagowabair@users.noreply.github.com>
2024-03-23 09:02:52 +0800

    Part 7 - Finalize Booking POST

--- Bookly.Web/Controllers/BookingController.cs
+++ Bookly.Web/Controllers/BookingController.cs
@@ -1,4 +1,5 @@
 ﻿using Bookly.Application.Common.Interfaces;
+using Bookly.Application.Common.Utility;
 using Bookly.Domain.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -37,6 +38,27 @@ namespace Bookly.Web.Controllers
             booking.TotalCost = booking.Villa.Price * nights;
 
             return View(booking);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult FinalizeBooking(Booking booking)
+        {
+            var villa = _unitOfWork.Villa.Get(u => u.Id == booking.VillaId);
+            booking.TotalCost = booking.Villa.Price * booking.Nights;
+
+            booking.Status = SD.StatusPending;
+            booking.BookingDate = DateTime.Now;
+
+            _unitOfWork.Booking.Add(booking);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(BookingConfirmation), new { bookingId = booking.Id });
+        }
+
+        [Authorize]
+        public IActionResult BookingConfirmation(int bookingId)
+        {
+            return View(bookingId);
         }
     }
 }
