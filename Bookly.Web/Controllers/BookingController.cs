ca2016fe5b7e6cfc4e6a295b635f5b9d77b46c13
MohammedBagowabair <159595929+MohammedBagowabair@users.noreply.github.com>
2024-03-23 08:19:04 +0800

    Part 7 - Get Action Finalize Booking

---
+++ Bookly.Web/Controllers/BookingController.cs
@@ -0,0 +1,29 @@
+﻿using Bookly.Application.Common.Interfaces;
+using Bookly.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookly.Web.Controllers
+{
+    public class BookingController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public BookingController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult FinalizeBooking(int villaId, DateOnly checkInDate, int nights)
+        {
+            Booking booking = new()
+            {
+                VillaId = villaId,
+                Villa = _unitOfWork.Villa.Get(u => u.Id == villaId, includeProperties: "VillaAmenity"),
+                CheckInDate = checkInDate,
+                Nights = nights,
+                CheckOutDate = checkInDate.AddDays(nights),
+            };
+
+            return View(booking);
+        }
+    }
+}
