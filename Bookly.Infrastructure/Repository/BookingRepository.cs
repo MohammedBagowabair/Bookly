9b53fb34841fd60eb4f82221c0a73cc0f880e8c8
MohammedBagowabair <159595929+MohammedBagowabair@users.noreply.github.com>
2024-03-23 08:14:19 +0800

    Part 7 - Create Booking Table and Booking Repository

---
+++ Bookly.Infrastructure/Repository/BookingRepository.cs
@@ -0,0 +1,26 @@
+﻿using Bookly.Application.Common.Interfaces;
+using Bookly.Domain.Entities;
+using Bookly.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookly.Infrastructure.Repository
+{
+    public class BookingRepository : Repository<Booking>, IBookingRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BookingRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Booking entity)
+        {
+            _db.Bookings.Update(entity);
+        }
+    }
+}
